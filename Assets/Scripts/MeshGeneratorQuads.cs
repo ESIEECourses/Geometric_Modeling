d9150edb01c0f8052fdcdaaa26ebdce439a29bb6
gabriel.leroux@edu.esiee.fr <gabriel.leroux@edu.esiee.fr>
2022-11-15 09:25:31 +0100

    First commit HalfEdge

--- Assets/Scripts/MeshGeneratorQuads.cs
+++ Assets/Scripts/MeshGeneratorQuads.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using WingedEdge;
+
+using Unity.Mathematics;
+using static Unity.Mathematics.math;
+
+delegate Vector3 ComputePosDelegate(float kX, float kZ);
+delegate float3 ComputePosDelegate_SIMD(float3 k);
 
 public class MeshGeneratorQuads : MonoBehaviour
 {
@@ -86,6 +91,156 @@ public class MeshGeneratorQuads : MonoBehaviour
         //         return OOmega + OmegaP;
         //     }
         // );
+        /*
+        //Helix
+
+        m_Mf.mesh = CreateNormalizedGridXZ(20 * 6, 10,
+
+            (kX, kZ) =>
+
+            {
+
+                float R = 3;
+
+                float r = 1;
+
+                float theta = 6 * 2 * Mathf.PI * kX;
+
+                Vector3 OOmega = new Vector3(R * Mathf.Cos(theta), 0, R * Mathf.Sin(theta));
+
+
+
+                float alpha = Mathf.PI * 2 * kZ;
+
+                Vector3 OmegaP = r * Mathf.Cos(alpha) * OOmega.normalized + r * Mathf.Sin(alpha) * Vector3.up
+
+                                   + Vector3.up * kX * 2 * r * 6;
+
+                return OOmega + OmegaP;
+
+            }
+
+        );
+        */
+        
+
+
+
+        // Unity.Mathematics
+
+        /*
+
+        bool bothSides = true;
+
+
+
+        m_Mf.mesh = CreateNormalizedGridXZ_SIMD(
+
+            (bothSides ? 2 : 1) * int3(100, 100, 1),
+
+            (k) =>
+
+            {
+
+                if (bothSides) k = abs((k - .5f) * 2);
+
+
+
+                //return lerp(float3(-5f, 0, -5f), float3(5f, 0, 5f), k.xzy);
+
+                //return lerp(float3(-5, 1, -5), float3(5, 0, 5), float3(k.x, step(.2f, k.x), k.y)) ;
+
+                //return lerp(float3(-5, 1, -5), float3(5, 0, 5), float3(k.x, smoothstep(.2f - 0.05f, .2f + 0.05f, k.x), k.y)) ;
+
+                //return lerp(float3(-5, 1, -5), float3(5, 0, 5), float3(k.x, smoothstep(0.2f - .05f, .2f + .05f, k.x * k.y), k.y));
+
+                return lerp(float3(-5, 1, -5), float3(5, 0, 5), float3(
+
+                    k.x,
+
+                    0.5f * (sin(k.x * 2 * PI * 4) * cos(k.y * 2 * PI * 3) + 1),
+
+                    //smoothstep(0.2f - .05f, .2f + .05f, 0.5f*(sin(k.x*2*PI*4) * cos(k.y*2*PI*3)+1))
+
+                     k.y));
+
+            }
+
+            );
+
+        */
+
+
+
+        // repeated pattern
+
+        int3 nCells = int3(3, 3, 1);
+
+        int3 nSegmentsPerCell = int3(100, 100, 1);
+
+        float3 kStep = float3(1) / (nCells * nSegmentsPerCell);
+
+
+
+        float3 cellSize = float3(1, .5f, 1);
+
+
+
+        m_Mf.mesh = CreateNormalizedGridXZ_SIMD(
+
+            nCells * nSegmentsPerCell,
+
+            (k) =>
+
+            {
+
+                // calculs sur la grille normalisée
+
+                int3 index = (int3)floor(k / kStep);
+
+                int3 localIndex = index % nSegmentsPerCell;
+
+                int3 indexCell = index / nSegmentsPerCell;
+
+                float3 relIndexCell = (float3)indexCell / nCells;
+
+
+
+                // calculs sur les positions dans l'espace
+
+                /*
+
+                float3 cellOriginPos = lerp(
+
+                    -cellSize * nCells.xzy * .5f,
+
+                    cellSize * nCells.xzy * .5f,
+
+                    relIndexCell.xzy);
+
+                */
+
+
+
+                float3 cellOriginPos = floor(k * nCells).xzy; // Theo's style ... ne prend pas en compte cellSize
+
+
+
+                k = frac(k * nCells);
+
+
+
+                return cellOriginPos
+
+                + cellSize * float3(k.x, smoothstep(0.2f - .05f, .2f + .05f, k.x * k.y), k.y);
+
+
+
+            }
+
+            );
+
+
         /*GUIUtility.systemCopyBuffer = ConvertToCSV("\t");
         Debug.Log(ConvertToCSV("\t"));*/
 
@@ -254,6 +409,50 @@ public class MeshGeneratorQuads : MonoBehaviour
         mesh.SetIndices(quads, MeshTopology.Quads, 0);
 
         return mesh;
+    }
+
+    Mesh CreateNormalizedGridXZ_SIMD(int3 nSegments, ComputePosDelegate_SIMD computePos = null)
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "normalizedGrid";
+        Vector3[] vertices = new Vector3[(nSegments.x + 1) * (nSegments.y + 1)];
+        int[] quads = new int[nSegments.x * nSegments.y * 4];
+
+        //Vertices
+
+        int index = 0;
+
+        for (int i = 0; i < nSegments.y + 1; i++)
+        {
+            for (int j = 0; j < nSegments.x + 1; j++)
+            {
+                float3 k = float3(j, i, 0) / nSegments;
+                vertices[index++] = computePos != null ? computePos(k) : k;
+            }
+        }
+
+        index = 0;
+        int offset = 0;
+        int offsetNextLine = offset;
+
+        //Quads
+        for (int i = 0; i < nSegments.y; i++)
+        {
+            offsetNextLine += nSegments.x + 1;
+            for (int j = 0; j < nSegments.x; j++)
+
+            {
+                quads[index++] = offset + j;
+                quads[index++] = offsetNextLine + j;
+                quads[index++] = offsetNextLine + j + 1;
+                quads[index++] = offset + j + 1;
+            }
+            offset = offsetNextLine;
+        }
+        mesh.vertices = vertices;
+        mesh.SetIndices(quads, MeshTopology.Quads, 0);
+        return mesh;
+
     }
 
     Mesh CreateBox(Vector3 halfSize)
